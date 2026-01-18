80ba0e903f4d7654c2ab6ce037d55d2758938d5b
Gautam Halvadiya <halvadiyagautam57@gmail.com>
2026-01-18 22:32:05 +0530

    Add product page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 
 export const ShopContext = createContext();
 
@@ -9,10 +10,60 @@ const ShopContextProvider = (props) => {
     const delivery_free = 10;
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({})
+    
+    const addToCart = (itemId, size) => {
+        
+        let cartData = structuredClone(cartItems)
+
+        if(!size)
+        {
+            toast.error('Please select product size')
+            return
+        }
+
+        if(cartData[itemId])
+        {
+            if(cartData[itemId][size])
+                cartData[itemId][size] += 1
+            else
+                cartData[itemId][size] = 1
+
+        }
+        else
+        {
+            cartData[itemId] = {}
+            cartData[itemId][size] = 1
+        }
+        setCartItems(cartData)
+        
+    }
+
+    let getCartCount = () => {
+        let totalCount = 0
+
+        for (const productId in cartItems) {
+            const sizes = cartItems[productId]
+
+            for (const size in sizes) {
+                totalCount += sizes[size]
+            }
+        }
+
+        return totalCount
+
+    }
 
     const value = {
-        products, currency, delivery_free, search, setSearch, showSearch, setShowSearch
+        products, currency, delivery_free, search, setSearch, showSearch, setShowSearch,cartItems, addToCart, getCartCount
     }
+
+    useEffect(() => {
+        console.log(cartItems)
+    }, [cartItems])
+
+    
+    
 
     return (
         <ShopContext.Provider value={value}>
