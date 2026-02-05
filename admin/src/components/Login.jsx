f50f6a5f6b9420e728e050ad3498df4a8746ef52
Gautam Halvadiya <halvadiyagautam57@gmail.com>
2026-02-05 18:27:41 +0530

    Add item add page in admin panel with JWT auth.

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-function Login() {
+function Login({setToken}) {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
@@ -11,10 +12,18 @@ function Login() {
       try {
         e.preventDefault()
         const response = await axios.post(backendUrl + '/api/user/admin', {email, password})
-        console.log(response)
+        
+        if(response.data.success)
+        {
+          setToken(response.data.token)
+        }else{
+          toast.error(response.data.message)
+
+        }
         
       } catch (error) {
-        
+        console.log(error);
+        toast.error(error.message)
       }
   }
 
